d3c07b5398b4dd5cd7a9f47623a68bfa7383c78d
GOMA PETER ORAH <peterorah40@gmail.com>
2025-07-25 21:51:55 +0100

    Delete SpringBoot-Project/terraform directory

--- SpringBoot-Project/terraform/modules/compute/keypair.tf
+++
@@ -1,9 +0,0 @@
- resource "tls_private_key" "ec2_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "generated_key" {
-  key_name   = "springboot_key"
-  public_key = tls_private_key.ec2_key.public_key_openssh
-}
